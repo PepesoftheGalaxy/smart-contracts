f268854dae09d3602f8a48e6a927fc7768c57b86
Galactic Pepe <131546787+galacticpepe@users.noreply.github.com>
2023-05-26 18:16:21 -0700

    check in code

--- PepesOfTheGalaxy.sol
+++ PepesOfTheGalaxy.sol
@@ -2,30 +2,23 @@
 
 pragma solidity ^0.8.0;
 
-// Importing required OpenZeppelin contracts
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";  // ERC20 interface
-import "@openzeppelin/contracts/access/Ownable.sol";  // Owner functionality
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-// The main contract for the PepesOfTheGalaxy token
 contract PepesOfTheGalaxy is Ownable, ERC20 {
-
-    // The maximum and minimum amount of tokens that can be held by an address
     uint256 public maxHoldingAmount;
     uint256 public minHoldingAmount;
-
-    // Address of the Uniswap pair for this token
     address public uniswapV2Pair;
-
-    // Indicates if trading limit is active
     bool public limited;
+    uint256 private tokenCap;
 
-    // The constructor function runs once upon contract creation
-    constructor(uint256 _totalSupply) ERC20("Pepes of the Galaxy", "PEPEG") {
-        // Mint the total supply to the creator of the contract
+    // Constructor function that initializes the contract
+    constructor(uint256 _totalSupply, uint256 _tokenCap) ERC20("Pepes of the Galaxy", "PEPEG") {
         _mint(msg.sender, _totalSupply);
+        tokenCap = _tokenCap;
     }
 
-    // Function to set rules for trading. Only callable by the owner
+    // Function to set trading rules for the contract, callable only by the owner
     function setRule(bool _limited, address _uniswapV2Pair, uint256 _maxHoldingAmount, uint256 _minHoldingAmount) external onlyOwner {
         limited = _limited;
         uniswapV2Pair = _uniswapV2Pair;
@@ -33,12 +26,8 @@ contract PepesOfTheGalaxy is Ownable, ERC20 {
         minHoldingAmount = _minHoldingAmount;
     }
 
-    // Function that runs before any token transfer. Overridden from ERC20 contract
-    function _beforeTokenTransfer(
-        address from,
-        address to,
-        uint256 amount
-    ) override internal virtual {
+    // Internal function that runs before any token transfer
+    function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual override {
         // If the Uniswap pair has not been set, only allow transfers from or to the owner
         if (uniswapV2Pair == address(0)) {
             require(from == owner() || to == owner(), "trading is not started");
@@ -50,8 +39,21 @@ contract PepesOfTheGalaxy is Ownable, ERC20 {
         }
     }
 
-    // Public function to burn tokens from caller's balance
+    // External function to burn tokens from caller's balance
     function burn(uint256 value) external {
         _burn(msg.sender, value);
+    }
+
+// Function to transfer ownership of the contract to a new address, callable only by the owner
+function transferOwnership(address newOwner) external onlyOwner {
+    require(newOwner != address(0), "Invalid new owner");
+    super.transferOwnership(newOwner);
+}
+
+    // Function to mint new tokens, callable only by the owner
+    function mint(address account, uint256 amount) external onlyOwner {
+        // Check if the token cap would be exceeded by minting new tokens
+        require(totalSupply() + amount <= tokenCap, "Token cap exceeded");
+        _mint(account, amount);
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# token
- 
+# PepesOfTheGalaxy Smart Contract
+
+## Overview
+
+The PepesOfTheGalaxy contract is a custom ERC20 token contract, built with [OpenZeppelin](https://openzeppelin.com/) libraries. This contract allows the creation of a token with the name "Pepes of the Galaxy" and the symbol "PEPEG". It also includes additional features such as token burning, ownership transfer, and trading rules.
+
+## Features
+
+- **ERC20 Token**: The contract inherits from OpenZeppelin's ERC20 contract, which provides standard functions for a [ERC20 token](https://ethereum.org/en/developers/docs/standards/tokens/erc-20/).
+
+- **Ownable**: The contract also inherits from OpenZeppelin's Ownable contract. This contract provides basic access control mechanism, where there is an account (an owner) that can be granted exclusive access to certain functions.
+
+- **Token Cap**: A cap on the total number of tokens that can be minted is implemented to prevent unlimited minting of tokens.
+
+- **Trading Rules**: The contract includes an option to set trading rules which can limit the minimum and maximum amount of tokens that can be held by an address. This rule can be turned on and off.
+
+- **Token Burning**: Users have the ability to burn their tokens, permanently removing them from the total supply.
+
+## Functions
+
+- **constructor(uint256 _totalSupply, uint256 _tokenCap)**: Initializes the contract, mints the total supply of tokens to the message sender, and sets the token cap.
+
+- **setRule(bool _limited, address _uniswapV2Pair, uint256 _maxHoldingAmount, uint256 _minHoldingAmount)**: Sets the trading rules for the contract. This function can only be called by the owner of the contract.
+
+- **_beforeTokenTransfer(address from, address to, uint256 amount)**: A hook that runs before any token transfer. It checks the trading rules before allowing a transfer.
+
+- **burn(uint256 value)**: Burns the specified number of tokens from the message sender's account.
+
+- **transferOwnership(address newOwner)**: Transfers ownership of the contract to a new address. This function can only be called by the current owner of the contract.
+
+- **mint(address account, uint256 amount)**: Mints new tokens to a specific address. This function can only be called by the owner of the contract.
+
+## Security
+
+The contract is built using well-audited OpenZeppelin contracts which follow best practices for contract security.
