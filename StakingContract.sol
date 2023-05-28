18165037c1d997fb5476df5c96482cd0e39f8684
Galactic Pepe <131546787+galacticpepe@users.noreply.github.com>
2023-05-28 13:01:31 -0700

    add get remainingStakeTime function

--- StakingContract.sol
+++ StakingContract.sol
@@ -1,57 +1,85 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
-contract StakingContract is ERC721Holder {
-    IERC721 public nftAddress;
-    address public PEPEG_TOKEN_ADDRESS;
-    uint256 public constant STAKING_PERIOD = 7 days;
+contract PepeStaking is ReentrancyGuard {
+    using SafeMath for uint256;
 
-    struct Stake {
-        uint256 tokenId;
-        uint256 timeStaked;
+    struct StakeInfo {
+        uint256 stakedTime;
+        bool staked;
     }
 
-    mapping (address => Stake) private _stakes;
-    mapping (address => uint256) private _totalRewards;
+    IERC721 public pepeNFT;
+    IERC20 public pepeToken;
 
-    event Staked(address indexed user, uint256 tokenId, uint256 timestamp);
-    event Withdrawn(address indexed user, uint256 tokenId, uint256 reward, uint256 timestamp);
-    event RewardPaid(address indexed user, uint256 reward);
+    uint256 public minStakingTime = 7 days;
+    uint256 public rewardAmount = 10000 * (10 ** 18);  // 10000 tokens with 18 decimals
+    uint256 public totalRewards;
 
-    constructor(address _nftAddress, address _pepegTokenAddress) {
-        nftAddress = IERC721(_nftAddress);
-        PEPEG_TOKEN_ADDRESS = _pepegTokenAddress;
+    mapping(address => StakeInfo) public stakingInfo;
+    mapping(address => uint256) public userRewards;
+
+    event Stake(address indexed user, uint256 time);
+    event Withdraw(address indexed user, uint256 time, uint256 reward);
+    event RewardIncreased(address indexed user, uint256 reward);
+    event RewardDecreased(address indexed user, uint256 reward);
+
+    constructor(address _pepeNFT, address _pepeToken) {
+        pepeNFT = IERC721(_pepeNFT);
+        pepeToken = IERC20(_pepeToken);
     }
 
-    function stakeNFT(uint256 _tokenId) external {
-        nftAddress.safeTransferFrom(msg.sender, address(this), _tokenId);
-        _stakes[msg.sender] = Stake(_tokenId, block.timestamp);
-        emit Staked(msg.sender, _tokenId, block.timestamp);
+    function stakeNFT(uint256 tokenId) external {
+        require(pepeNFT.ownerOf(tokenId) == msg.sender, "Not owner of this NFT");
+        require(!stakingInfo[msg.sender].staked, "Already staking an NFT");
+
+        // Transfer the NFT to this contract
+        pepeNFT.transferFrom(msg.sender, address(this), tokenId);
+
+        // Update staking info
+        stakingInfo[msg.sender] = StakeInfo(block.timestamp, true);
+
+        emit Stake(msg.sender, block.timestamp);
     }
 
-    function withdrawNFT(uint256 _tokenId) external {
-        require(_stakes[msg.sender].tokenId == _tokenId, "NFT not staked");
-        require(block.timestamp >= _stakes[msg.sender].timeStaked + STAKING_PERIOD, "Staking period not over");
+    function withdrawNFT(uint256 tokenId) external nonReentrant {
+        require(pepeNFT.ownerOf(tokenId) == address(this), "Contract is not owner of this NFT");
+        require(stakingInfo[msg.sender].staked, "Not staking an NFT");
+        require(block.timestamp >= stakingInfo[msg.sender].stakedTime.add(minStakingTime), "Minimum staking time not reached");
 
-        uint256 reward = 10000; // amount of PEPEG tokens rewarded
+        // Transfer the NFT back to the user
+        pepeNFT.transferFrom(address(this), msg.sender, tokenId);
 
-        // Update the user's total accumulated rewards.
-        _totalRewards[msg.sender] -= reward;
+        // Update staking info
+        stakingInfo[msg.sender].staked = false;
 
-        IERC20(PEPEG_TOKEN_ADDRESS).transfer(msg.sender, reward);
-        emit RewardPaid(msg.sender, reward);
+        // Transfer the reward
+        pepeToken.transfer(msg.sender, userRewards[msg.sender]);
 
-        _stakes[msg.sender].tokenId = 0;
-        _stakes[msg.sender].timeStaked = 0;
+        emit Withdraw(msg.sender, block.timestamp, userRewards[msg.sender]);
 
-        nftAddress.safeTransferFrom(address(this), msg.sender, _tokenId);
-        emit Withdrawn(msg.sender, _tokenId, reward, block.timestamp);
+        // Decrease the total rewards and user rewards
+        totalRewards = totalRewards.sub(userRewards[msg.sender]);
+        userRewards[msg.sender] = 0;
+
+        emit RewardDecreased(msg.sender, userRewards[msg.sender]);
     }
 
-    function totalRewards(address user) external view returns (uint256) {
-        return _totalRewards[user];
+    function getRemainingStakeTime(address user) public view returns (uint256) {
+        if(block.timestamp >= stakingInfo[user].stakedTime.add(minStakingTime)) {
+            return 0;
+        } else {
+            return stakingInfo[user].stakedTime.add(minStakingTime).sub(block.timestamp);
+        }
     }
-}
+
+    function increaseRewards(address user) external {
+        require(userRewards[user] < rewardAmount, "Already received full rewards");
+
+        userRewards[user] = rewardAmount;
+       quote("Here's the updated contract with", "userRewards[user] = rewardAmount;")
