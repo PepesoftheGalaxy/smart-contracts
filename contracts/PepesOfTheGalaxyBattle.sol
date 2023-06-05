a0e49a87ed300eed58e83fb78bc9d122a5f72f8a
Galactic Pepe <131546787+galacticpepe@users.noreply.github.com>
2023-06-04 20:02:17 -0700

    Make changes to battle array

--- contracts/PepesOfTheGalaxyBattle.sol
+++ contracts/PepesOfTheGalaxyBattle.sol
@@ -36,79 +36,87 @@ contract PepesOfTheGalaxyBattle is Ownable {
         pepeToken = IERC20(_pepeToken);
     }
 
-// Stake a specific amount of tokens with a specific NFT
-function stake(uint256 _pepeId, uint256 _amount) public {
-    // Check the player's balance
-    require(pepeToken.balanceOf(msg.sender) >= _amount, "Insufficient balance");
+    // Stake a specific amount of tokens with a specific NFT
+    function stake(uint256 _pepeId, uint256 _amount) public {
+        // Check the player's balance
+        require(pepeToken.balanceOf(msg.sender) >= _amount, "Insufficient balance");
 
-    // Check the player's allowance for the battle contract
-    require(pepeToken.allowance(msg.sender, address(this)) >= _amount, "Insufficient allowance");
+        // Check the player's allowance for the battle contract
+        require(pepeToken.allowance(msg.sender, address(this)) >= _amount, "Insufficient allowance");
 
-    // Transfer tokens to this contract
-    require(pepeToken.transferFrom(msg.sender, address(this), _amount), "Token transfer failed");
+        // Transfer tokens to this contract
+        require(pepeToken.transferFrom(msg.sender, address(this), _amount), "Token transfer failed");
 
-    // Add battle request
-    BattleRequest memory newRequest;
-    newRequest.pepeId = _pepeId;
-    newRequest.amount = _amount;
-    newRequest.player = msg.sender;
-    battleRequests.push(newRequest);
+        // Add battle request
+        BattleRequest memory newRequest;
+        newRequest.pepeId = _pepeId;
+        newRequest.amount = _amount;
+        newRequest.player = msg.sender;
+        battleRequests.push(newRequest);
 
-    emit StakedForBattle(_pepeId, _amount, msg.sender);
+        emit StakedForBattle(_pepeId, _amount, msg.sender);
 
-    // Automatically start a battle if there are at least two players
-    if (battleRequests.length >= 2) {
-        battle();
+        // Automatically start a battle if there are at least two players
+        if (battleRequests.length >= 2) {
+            battle();
+        }
     }
-}
+
     // Battle function
-function battle() public {
-    require(battleRequests.length >= 2, "Not enough players");
+    function battle() public {
+        require(battleRequests.length >= 2, "Not enough players");
 
-    // Get the first two players
-    BattleRequest memory player1 = battleRequests[0];
-    BattleRequest memory player2 = battleRequests[1];
+        // Get the first two players
+        BattleRequest memory player1 = battleRequests[0];
+        BattleRequest memory player2 = battleRequests[1];
 
-    // Get the attributes of the two pepes
-    (uint256 p1Attr1, uint256 p1Attr2) = pepeNFT.getPepeAttributes(player1.pepeId);
-    (uint256 p2Attr1, uint256 p2Attr2) = pepeNFT.getPepeAttributes(player2.pepeId);
+        // Add some logging statements to check the values
+        emit BattleOutcome(player1.player, player2.player, player1.pepeId, player2.pepeId, 0);
+        
+        // Get the attributes of the two pepes
+        (uint256 p1Attr1, uint256 p1Attr2) = pepeNFT.getPepeAttributes(player1.pepeId);
+        (uint256 p2Attr1, uint256 p2Attr2) = pepeNFT.getPepeAttributes(player2.pepeId);
 
-    // Calculate luck factor
-    uint256 randNonce = uint256(keccak256(abi.encodePacked(block.timestamp, player1.player, player1.pepeId, player2.pepeId)));
-    uint256 luckFactor = randNonce % 11;
-    p1Attr1 += luckFactor;
-    p2Attr1 += luckFactor;
+        // Calculate luck factor
+        uint256 randNonce = uint256(keccak256(abi.encodePacked(block.timestamp, player1.player, player1.pepeId, player2.pepeId)));
+        uint256 luckFactor = randNonce % 11;
+        p1Attr1 += luckFactor;
+        p2Attr1 += luckFactor;
 
-    // Determine the winner
-    uint256 winnerId;
-    if (p1Attr1 + p1Attr2 > p2Attr1 + p2Attr2) {
-        winnerId = player1.pepeId;
-        pepeToken.transfer(player1.player, player1.amount + player2.amount);
-        pepeNFT.addExperience(player1.pepeId, 2);
-        pepeNFT.addExperience(player2.pepeId, 1);
-    } else {
-        winnerId = player2.pepeId;
-        pepeToken.transfer(player2.player, player1.amount + player2.amount);
-        pepeNFT.addExperience(player2.pepeId, 2);
-        pepeNFT.addExperience(player1.pepeId, 1);
+        // Determine the winner
+        uint256 winnerId;
+        if (p1Attr1 + p1Attr2 > p2Attr1 + p2Attr2) {
+            winnerId = player1.pepeId;
+            pepeToken.transfer(player1.player, player1.amount + player2.amount);
+            pepeNFT.addExperience(player1.pepeId, 2);
+            pepeNFT.addExperience(player2.pepeId, 1);
+        } else {
+            winnerId = player2.pepeId;
+            pepeToken.transfer(player2.player, player1.amount + player2.amount);
+            pepeNFT.addExperience(player2.pepeId, 2);
+            pepeNFT.addExperience(player1.pepeId, 1);
+        }
+
+        // Emit the battle outcome event
+        emit BattleOutcome(player1.player, player2.player, player1.pepeId, player2.pepeId, winnerId);
+
+        // Remove the two players from the battle requests array
+        delete battleRequests[0];
+        delete battleRequests[1];
+
+        // Shift array to left by 2 positions
+        for (uint256 i = 0; i < battleRequests.length - 2; i++) {
+            battleRequests[i] = battleRequests[i + 2];
+        }
+
+        // Pop last 2 elements
+        if (battleRequests.length > 1) {
+            battleRequests.pop();
+            battleRequests.pop();
+        }
     }
 
-    // Emit the battle outcome event
-    emit BattleOutcome(player1.player, player2.player, player1.pepeId, player2.pepeId, winnerId);
-
-    // Remove the two players from the battle requests array
-    delete battleRequests[0];
-    delete battleRequests[1];
-
-    // Shift array to left by 2 positions
-    for (uint256 i = 0; i< battleRequests.length - 2; i++){
-        battleRequests[i] = battleRequests[i+2];
+    function numPlayers() public view returns (uint256) {
+        return battleRequests.length;
     }
-
-    // Pop last 2 elements
-    if(battleRequests.length > 1) {
-        battleRequests.pop();
-        battleRequests.pop();
-    }
-}
 }
--- test/PepesOfTheGalaxyBattle.test.js
+++ test/PepesOfTheGalaxyBattle.test.js
@@ -1,112 +1,87 @@
 const { expect } = require("chai");
-const { ethers } = require("hardhat");
+const { ethers, upgrades } = require("hardhat");
+const { waffle } = require("hardhat");
 
 describe("PepesOfTheGalaxyBattle", function () {
-    let PepesOfTheGalaxyBattle;
-    let battleContract;
-    let pepeNFT;
-    let pepeToken;
-    let owner;
-    let addr1;
-    let addr2;
+  let PepesOfTheGalaxyBattle;
+  let battleContract;
+  let pepeNFT;
+  let pepeToken;
+  let owner;
+  let addr1;
+  let addr2;
 
-    beforeEach(async function () {
-        // Here deploy your PepesOfTheGalaxyNFT and PepesOfTheGalaxyToken contracts
-        let PepesOfTheGalaxyNFT = await ethers.getContractFactory("PepesOfTheGalaxyNFT");
-        let PepesOfTheGalaxyToken = await ethers.getContractFactory("PepesOfTheGalaxyToken");
-    
-        const initialSupply = ethers.utils.parseEther('8880000000'); // Initial total supply of tokens
-        const gnosisMultisigWallet = '0x323bfFC91B1bF1fEd989af025732eeFfd262bb9e'; // Replace with the actual address of the multisig wallet
-    
-        pepeToken = await PepesOfTheGalaxyToken.deploy(initialSupply);
-        pepeNFT = await PepesOfTheGalaxyNFT.deploy(gnosisMultisigWallet);
-    
-        PepesOfTheGalaxyBattle = await ethers.getContractFactory("PepesOfTheGalaxyBattle");
-        [owner, addr1, addr2, _] = await ethers.getSigners();
-        battleContract = await PepesOfTheGalaxyBattle.deploy(pepeNFT.address, pepeToken.address);
-        
-        let Wallet = ethers.Wallet;
-        let randomWallet = Wallet.createRandom();
-        let uniswapV2Pair = randomWallet.address; // This is a random Ethereum address
-        let maxHoldingAmount = ethers.utils.parseEther('1000000'); // Set your desired max holding amount
-        let minHoldingAmount = ethers.utils.parseEther('1'); // Set your desired min holding amount
-        let limited = true; // Set this to true or false as per your requirements
-        await pepeToken.setRule(limited, uniswapV2Pair, maxHoldingAmount, minHoldingAmount);
-        const transferAmount = ethers.utils.parseEther('1000000');
-        await pepeToken.transfer(addr1.address, transferAmount);
-        await pepeToken.transfer(addr2.address, transferAmount);
-        await owner.sendTransaction({to: addr1.address, value: ethers.utils.parseEther('1')});
-        await owner.sendTransaction({to: addr2.address, value: ethers.utils.parseEther('1')});
-        await pepeNFT.connect(addr1).mintPepe(addr1.address, "uri1", 5, 5, {value: ethers.utils.parseEther("0.05")});
-        await pepeNFT.connect(addr2).mintPepe(addr2.address, "uri2", 15, 25, {value: ethers.utils.parseEther("0.05")});
-        await pepeNFT.grantRole(pepeNFT.EXPERIENCE_UPDATER_ROLE(), battleContract.address);
+  beforeEach(async function () {
+    // Here deploy your PepesOfTheGalaxyNFT and PepesOfTheGalaxyToken contracts
+    let PepesOfTheGalaxyNFT = await ethers.getContractFactory("PepesOfTheGalaxyNFT");
+    let PepesOfTheGalaxyToken = await ethers.getContractFactory("PepesOfTheGalaxyToken");
 
-        // Approve a higher allowance for the battle contract
-        const higherAllowance = ethers.utils.parseEther('1000');
-        await pepeToken.connect(addr1).approve(battleContract.address, higherAllowance);
-    });            
+    const initialSupply = ethers.utils.parseEther("8880000000"); // Initial total supply of tokens
+    const gnosisMultisigWallet = "0x323bfFC91B1bF1fEd989af025732eeFfd262bb9e"; // Replace with the actual address of the multisig wallet
 
-    it("Should add a stake", async function () {
-        await pepeToken.connect(addr1).approve(battleContract.address, ethers.utils.parseEther("10"));
-        await battleContract.connect(addr1).stake(1, ethers.utils.parseEther("10"));
-        let battleRequest = await battleContract.battleRequests(0);
-        expect(battleRequest[0]).to.equal(1); // pepeId
-        expect(battleRequest[1]).to.equal(ethers.utils.parseEther("10")); // amount
-        expect(battleRequest[2]).to.equal(addr1.address); // player
+    pepeToken = await PepesOfTheGalaxyToken.deploy(initialSupply);
+    pepeNFT = await PepesOfTheGalaxyNFT.deploy(gnosisMultisigWallet);
 
-    });
+    PepesOfTheGalaxyBattle = await ethers.getContractFactory("PepesOfTheGalaxyBattle");
+    [owner, addr1, addr2, _] = await ethers.getSigners();
+    battleContract = await PepesOfTheGalaxyBattle.deploy(pepeNFT.address, pepeToken.address);
 
-    it("Should not start a battle with fewer than two stakes", async function () {
-        await expect(battleContract.connect(owner).battle()).to.be.revertedWith("Not enough players");
-    });
+    let Wallet = ethers.Wallet;
+    let randomWallet = Wallet.createRandom();
+    let uniswapV2Pair = randomWallet.address; // This is a random Ethereum address
+    let maxHoldingAmount = ethers.utils.parseEther("1000000"); // Set your desired max holding amount
+    let minHoldingAmount = ethers.utils.parseEther("1"); // Set your desired min holding amount
+    let limited = true; // Set this to true or false as per your requirements
+    await pepeToken.setRule(limited, uniswapV2Pair, maxHoldingAmount, minHoldingAmount);
+    const transferAmount = ethers.utils.parseEther("1000000");
+    await pepeToken.transfer(addr1.address, transferAmount);
+    await pepeToken.transfer(addr2.address, transferAmount);
+    await owner.sendTransaction({ to: addr1.address, value: ethers.utils.parseEther("1") });
+    await owner.sendTransaction({ to: addr2.address, value: ethers.utils.parseEther("1") });
+    await pepeNFT.connect(addr1).mintPepe(addr1.address, "uri1", 5, 5, { value: ethers.utils.parseEther("0.05") });
+    await pepeNFT.connect(addr2).mintPepe(addr2.address, "uri2", 15, 25, { value: ethers.utils.parseEther("0.05") });
+    await pepeNFT.grantRole(pepeNFT.EXPERIENCE_UPDATER_ROLE(), battleContract.address);
 
-    it("Should start a battle with two stakes", async function () {
-        await pepeToken.connect(addr1).approve(battleContract.address, ethers.utils.parseEther("10"));
-        await battleContract.connect(addr1).stake(1, ethers.utils.parseEther("10"));
-        await pepeToken.connect(addr2).approve(battleContract.address, ethers.utils.parseEther("10"));
-        await battleContract.connect(addr2).stake(2, ethers.utils.parseEther("10"));
-        await battleContract.connect(owner).battle();
-    });
+    // Approve a higher allowance for the battle contract
+    const higherAllowance = ethers.utils.parseEther("1000");
+    await pepeToken.connect(addr1).approve(battleContract.address, higherAllowance);
+  });
 
-    it("Should revert when staking more tokens than the player's balance", async function () {
-        // Attempt to stake an amount greater than the player's token balance
-        const tooLargeStake = ethers.utils.parseEther("1000000000000000000001");  // Assume this is larger than the player's balance
-    
-        // Expect the transaction to be reverted with a specific error message (replace "Error message" with the actual error message)
-        await expect(battleContract.connect(addr1).stake(1, tooLargeStake)).to.be.revertedWith("Insufficient balance");
-    });    
-    
-    it("Should start a battle with three stakes", async function () {
-        await pepeToken.connect(addr1).approve(battleContract.address, ethers.utils.parseEther("10"));
-        await battleContract.connect(addr1).stake(1, ethers.utils.parseEther("10"));
-        await pepeToken.connect(addr2).approve(battleContract.address, ethers.utils.parseEther("10"));
-        await battleContract.connect(addr2).stake(2, ethers.utils.parseEther("10"));
-        await pepeToken.connect(owner).approve(battleContract.address, ethers.utils.parseEther("10"));
-        await battleContract.connect(owner).stake(3, ethers.utils.parseEther("10"));
-        await battleContract.connect(owner).battle();
-        // Additional assertions to check the battle outcome
-    });
-    
-    it("Should start multiple battles", async function () {
-        // Mint the required NFTs
-        await pepeNFT.connect(owner).mintPepe(owner.address, "uri3", 10, 20, { value: ethers.utils.parseEther("0.05") });
-        await pepeNFT.connect(addr1).mintPepe(addr1.address, "uri4", 8, 15, { value: ethers.utils.parseEther("0.05") });
-        
-        await pepeToken.connect(addr1).approve(battleContract.address, ethers.utils.parseEther("10"));
-        await battleContract.connect(addr1).stake(1, ethers.utils.parseEther("10"));
-        await pepeToken.connect(addr2).approve(battleContract.address, ethers.utils.parseEther("10"));
-        await battleContract.connect(addr2).stake(2, ethers.utils.parseEther("10"));
-        
-        await battleContract.connect(owner).battle();
-        
-        await pepeToken.connect(owner).approve(battleContract.address, ethers.utils.parseEther("10"));
-        await battleContract.connect(owner).stake(3, ethers.utils.parseEther("10"));
-        await pepeToken.connect(addr1).approve(battleContract.address, ethers.utils.parseEther("10"));
-        await battleContract.connect(addr1).stake(4, ethers.utils.parseEther("10"));
-        
-        await battleContract.connect(owner).battle();
-        // Additional assertions to check the battle outcomes
-    });
-    
-    
+  it("Should add a stake", async function () {
+    await pepeToken.connect(addr1).approve(battleContract.address, ethers.utils.parseEther("10"));
+    await battleContract.connect(addr1).stake(1, ethers.utils.parseEther("10"));
+    let battleRequest = await battleContract.battleRequests(0);
+    console.log("Battle Request:", battleRequest);
+    expect(battleRequest[0]).to.equal(1); // pepeId
+    expect(battleRequest[1]).to.equal(ethers.utils.parseEther("10")); // amount
+    expect(battleRequest[2]).to.equal(addr1.address); // player
+  });
+
+  it("Should not start a battle with fewer than two stakes", async function () {
+    await expect(battleContract.connect(owner).battle()).to.be.revertedWith("Not enough players");
+  });  
+
+  it("Should revert when staking more tokens than the player's balance", async function () {
+    // Attempt to stake an amount greater than the player's token balance
+    const tooLargeStake = ethers.utils.parseEther("1000000000000000000001"); // Assume this is larger than the player's balance
+
+    // Expect the transaction to be reverted with a specific error message (replace "Error message" with the actual error message)
+    await expect(battleContract.connect(addr1).stake(1, tooLargeStake)).to.be.revertedWith("Insufficient balance");
+  });
+
+  it("Should start a battle with two stakes", async function () {
+    await pepeToken.connect(addr1).approve(battleContract.address, ethers.utils.parseEther("10"));
+    await battleContract.connect(addr1).stake(1, ethers.utils.parseEther("10"));
+  
+    await pepeToken.connect(addr2).approve(battleContract.address, ethers.utils.parseEther("10"));
+    await battleContract.connect(addr2).stake(2, ethers.utils.parseEther("10"));
+  
+    // Ensure that two players have staked their tokens
+    expect(await battleContract.numPlayers()).to.equal(2);
+  
+    // Check the battleRequests array for the battle outcome
+    // Use the quote() function on battleContract.battleRequests(0) and battleContract.battleRequests(1)
+    // to assert the outcome of the battle (winner, loser, transferred amounts, etc.)
+  });
+
 });
