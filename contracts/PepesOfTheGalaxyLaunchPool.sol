57b93a389f9d643e1c660fd98d64557c64ce80d1
Galactic Pepe <131546787+galacticpepe@users.noreply.github.com>
2023-11-06 14:57:42 -0700

    Add checks and balances

--- contracts/PepesOfTheGalaxyLaunchPool.sol
+++ contracts/PepesOfTheGalaxyLaunchPool.sol
@@ -2,8 +2,14 @@
 pragma solidity ^0.8.0;
 
 import "./PepesOfTheGalaxyToken.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
-contract PepesOfTheGalaxyLaunchPool {
+contract PepesOfTheGalaxyLaunchPool is Ownable, Pausable, ReentrancyGuard {
+    using SafeMath for uint256;
+
     mapping(address => uint256) public stakes;
     mapping(address => uint256) public stakeTime;
     uint256 public totalStaked;
@@ -14,33 +20,44 @@ contract PepesOfTheGalaxyLaunchPool {
         token = PepesOfTheGalaxyToken(tokenAddress);
     }
 
-    function stake() public payable {
+    function stake() public payable whenNotPaused nonReentrant {
+        require(msg.value > 0, "Must stake a positive amount");
+
         // Update the staker's stake and the total staked amount
-        stakes[msg.sender] += msg.value;
-        totalStaked += msg.value;
+        stakes[msg.sender] = stakes[msg.sender].add(msg.value);
+        totalStaked = totalStaked.add(msg.value);
 
         // Update the staker's stake time
         stakeTime[msg.sender] = block.timestamp;
     }
 
-    function claim() public {
+    function claim() public whenNotPaused nonReentrant {
         require(block.timestamp >= stakeTime[msg.sender] + STAKING_PERIOD, "Staking period not yet over");
 
-        // Calculate the staker's share of the PEPEOG tokens in the pool
-        uint256 reward = token.balanceOf(address(this)) * stakes[msg.sender] / totalStaked;
+        uint256 userStake = stakes[msg.sender];
+        require(userStake > 0, "No stake to claim");
+
+        // Calculate the staker's share of the PEP
+        uint256 reward = token.balanceOf(address(this)).mul(userStake).div(totalStaked);
 
         // Apply a bonus based on the time staked using a bonding curve
-        // This is a simple example of a linear bonding curve
-        // We can replace this with any function that increases with time
-        uint256 timeStaked = block.timestamp - stakeTime[msg.sender];
-        uint256 bonus = reward * timeStaked / STAKING_PERIOD;
-        reward += bonus;
+        uint256 timeStaked = block.timestamp.sub(stakeTime[msg.sender]);
+        uint256 bonus = reward.mul(timeStaked).div(STAKING_PERIOD);
+        reward = reward.add(bonus);
 
         // Update the staker's stake and the total staked amount
         stakes[msg.sender] = 0;
-        totalStaked -= stakes[msg.sender];
+        totalStaked = totalStaked.sub(userStake);
 
         // Transfer the reward to the staker
         token.transfer(msg.sender, reward);
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
     }
 }
