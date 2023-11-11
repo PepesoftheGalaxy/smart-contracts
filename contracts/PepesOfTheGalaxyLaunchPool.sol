811ad6f0ecbcef42810c2588f338345fa540ec38
Galactic Pepe <131546787+galacticpepe@users.noreply.github.com>
2023-11-11 08:48:06 -0800

    update to take staking start and end times

--- contracts/PepesOfTheGalaxyLaunchPool.sol
+++ contracts/PepesOfTheGalaxyLaunchPool.sol
@@ -11,50 +11,47 @@ contract PepesOfTheGalaxyLaunchPool is Ownable, Pausable, ReentrancyGuard {
     using SafeMath for uint256;
 
     mapping(address => uint256) public stakes;
-    mapping(address => uint256) public stakeTime;
     uint256 public totalStaked;
     PepesOfTheGalaxyToken public token;
     uint256 public constant STAKING_PERIOD = 7 days;
+    uint256 public stakingStart;
+    uint256 public stakingEnd;
 
-    constructor(address tokenAddress) {
+    constructor(address tokenAddress, uint256 _stakingStart) {
         token = PepesOfTheGalaxyToken(tokenAddress);
+        stakingStart = _stakingStart;
+        stakingEnd = stakingStart.add(STAKING_PERIOD);
     }
 
     function stake() public payable whenNotPaused nonReentrant {
+        require(block.timestamp >= stakingStart, "Staking period not started");
+        require(block.timestamp < stakingEnd, "Staking period ended");
         require(msg.value > 0, "Must stake a positive amount");
 
         // Update the staker's stake and the total staked amount
         stakes[msg.sender] = stakes[msg.sender].add(msg.value);
         totalStaked = totalStaked.add(msg.value);
-
-        // Update the staker's stake time
-        stakeTime[msg.sender] = block.timestamp;
     }
 
-function claimAndWithdraw() public whenNotPaused nonReentrant {
-    require(block.timestamp >= stakeTime[msg.sender] + STAKING_PERIOD, "Staking period not yet over");
+    function claimAndWithdraw() public whenNotPaused nonReentrant {
+        require(block.timestamp >= stakingEnd, "Staking period not yet over");
 
-    uint256 userStake = stakes[msg.sender];
-    require(userStake > 0, "No stake to claim");
+        uint256 userStake = stakes[msg.sender];
+        require(userStake > 0, "No stake to claim");
 
-    // Calculate the staker's share of the PEP
-    uint256 reward = token.balanceOf(address(this)).mul(userStake).div(totalStaked);
+        // Calculate the staker's share of the PEP
+        uint256 reward = token.balanceOf(address(this)).mul(userStake).div(totalStaked);
 
-    // Apply a bonus based on the time staked using a bonding curve
-    uint256 timeStaked = block.timestamp.sub(stakeTime[msg.sender]);
-    uint256 bonus = reward.mul(timeStaked).div(STAKING_PERIOD);
-    reward = reward.add(bonus);
+        // Update the staker's stake and the total staked amount
+        stakes[msg.sender] = 0;
+        totalStaked = totalStaked.sub(userStake);
 
-    // Update the staker's stake and the total staked amount
-    stakes[msg.sender] = 0;
-    totalStaked = totalStaked.sub(userStake);
+        // Transfer the reward to the staker
+        token.transfer(msg.sender, reward);
 
-    // Transfer the reward to the staker
-    token.transfer(msg.sender, reward);
-
-    // Transfer the staked BNB back to the staker
-    payable(msg.sender).transfer(userStake);
-}
+        // Transfer the staked BNB back to the staker
+        payable(msg.sender).transfer(userStake);
+    }
 
     function pause() public onlyOwner {
         _pause();
